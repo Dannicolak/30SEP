af9ddb1a38fc2ac430fbde7d38936829956ba95c
Dannicolak <90389855+Dannicolak@users.noreply.github.com>
2021-09-30 14:43:32 +0200

    Add files via upload

---
+++ Task 1 (30 sep,2021)/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Collaboration:University ");
+            String str=sc.nextLine();
+            String []words=str.split(" ");
+            int max=0;
+            String res="";
+            boolean imax = false;
+            for(int i = 0; imax;)
+            {
+                max=words[i].length();
+                res=words[i];
+            }
+        }
+        System.out.print("Maximum Length of the words is : ");
+        System.out.print("Maximum Length words is : ");
+    }
+}
+
+
+
